d64adb7e45bcdc355b0fe4c3d283d85b1f801048
walker <walker@urdev.xyz>
2019-02-04 05:47:20 -0500

    added hamburger menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './Navbar.css';
+import Hamburger from 'react-hamburgers';
+import './Hamburgers.css'
+
 
 class Navbar extends Component {
 
@@ -8,6 +11,7 @@ class Navbar extends Component {
 
     this.state = {
       scrollState: null,
+      isActive: false,
     }
   }
 
@@ -19,7 +23,7 @@ class Navbar extends Component {
        if(window.scrollY < 15){
            myclass = null;
        }else{
-           myclass = 'raise';
+           myclass = 'raise hamburgered';
        }
        this.setState({
           scrollState: myclass
@@ -29,7 +33,7 @@ class Navbar extends Component {
 
   generatePageLinks(pageNames) {
     let listItems = pageNames.map((pageNames) =>
-      <li>{pageNames.name}</li>
+      <a href='/#'><li>{pageNames.name}</li></a>
     );
     return (
       listItems
@@ -42,10 +46,20 @@ class Navbar extends Component {
         <div className={"Navbar-inner"}>
             <a className={'logo'} href='/#'>urdev</a>
 
-            <ul className='navbar-links'>
+            <ul className='desktop navbar-links'>
               {this.generatePageLinks(this.props.pages)}
             </ul>
-
+            
+            <div className='mobile'>
+              <Hamburger
+              active={this.state.active}
+              type="slider"
+              onClick={() => this.setState({ active: !this.state.active })}
+              />
+              <ul className='navbar-links'>
+                {this.generatePageLinks(this.props.pages)}
+              </ul>
+            </div>
         </div>
       </div>
     );
