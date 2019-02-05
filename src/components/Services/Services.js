85728a0a3613d13aee19b2c956c83429e3dbd00b
walker <walker@urdev.xyz>
2019-02-04 22:22:33 -0500

    service card styling

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -17,15 +17,29 @@ class Services extends React.Component {
                     <div className='Services-card-image'>
                         <img src={ appdev }/>
                     </div>
-                </div>
-                <div className={'Services-card'}>
-                <div className='Services-card-image'>
-                        <img src={ appdev }/>
+                    <div className='Service-card-content'>
+                        <h1>Web Dev</h1>
+                        <p>At urdev we use a combination of the latest technologies and development practices to deliver fast, easy-to-use, and scalable applications.</p>
                     </div>
                 </div>
                 <div className={'Services-card'}>
-                <div className='Services-card-image'>
+                    <div className='Services-card-image'>
                         <img src={ appdev }/>
+                    </div>
+                    <div className='Service-card-content'>
+                        <h1>App Dev</h1>
+                        <p>At urdev we use a combination of the latest technologies and development practices to deliver fast, easy-to-use, and scalable applications.</p>
+
+                    </div>
+                </div>
+                <div className={'Services-card'}>
+                    <div className='Services-card-image'>
+                        <img src={ appdev }/>
+                    </div>
+                    <div className='Service-card-content'>
+                        <h1>SEO</h1>
+                        <p>At urdev we use a combination of the latest technologies and development practices to deliver fast, easy-to-use, and scalable applications.</p>
+
                     </div>
                 </div>
             </div>
