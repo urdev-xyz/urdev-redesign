1f5acd298c52e402e6e1a56d2939b3394d40a8b0
walker <walker@urdev.xyz>
2019-02-05 17:18:38 -0500

    added arrow

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,7 +21,8 @@ class Header extends Component {
 
   render() {
     return (
-      <div style={{height: this.state.headerHeight - 60}} className="Header">
+      <section>
+      <div style={{height: this.state.headerHeight - 180}} className="Header">
         <div className={'Header-split Header-text'}>
             <h1>Development Made Simple.</h1>
             <p>At urdev we use a combination of the latest technologies and development practices to deliver fast, easy-to-use, and scalable applications.</p>
@@ -34,6 +35,14 @@ class Header extends Component {
             <img src={ hero }/>
         </div>
       </div>
+      <div className='arrow-container'>
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" width="60px" height="60px" viewBox="0 0 960 560" enable-background="new 0 0 960 560">
+      <g id="Rounded_Rectangle_33_copy_4_1_">
+        <path d="M480,344.181L268.869,131.889c-15.756-15.859-41.3-15.859-57.054,0c-15.754,15.857-15.754,41.57,0,57.431l237.632,238.937   c8.395,8.451,19.562,12.254,30.553,11.698c10.993,0.556,22.159-3.247,30.555-11.698l237.631-238.937   c15.756-15.86,15.756-41.571,0-57.431s-41.299-15.859-57.051,0L480,344.181z"/>
+      </g>
+      </svg>
+    </div>
+    </section>
     );
   }
 }
