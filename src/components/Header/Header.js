c7826f7a034272211a6bde87ef086587aae87582
walker <walker@urdev.xyz>
2019-02-05 19:54:37 -0500

    scroll indicator now fades out on scroll

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,6 +9,7 @@ class Header extends Component {
 
     this.state = {
       headerHeight: 'auto',
+      scrollState: '',
     }
 
   }
@@ -17,6 +18,17 @@ class Header extends Component {
     this.setState(
       {headerHeight: window.innerHeight}
     )
+      window.addEventListener('scroll', (event) => {
+        let myclass;
+        if(window.scrollY < 15){
+            myclass = null;
+        }else{
+            myclass = 'fadeout';
+        }
+        this.setState({
+            scrollState: myclass
+        })
+      });
   }
 
   render() {
@@ -35,8 +47,8 @@ class Header extends Component {
             <img src={ hero }/>
         </div>
       </div>
-      <div className='arrow-container'>
-      <svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" width="60px" height="60px" viewBox="0 0 960 560" enable-background="new 0 0 960 560">
+      <div className={`arrow-container ${ this.state.scrollState}`}>
+      <svg className={this.state.scrollState} xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" width="60px" height="60px" viewBox="0 0 960 560" enable-background="new 0 0 960 560">
       <g id="Rounded_Rectangle_33_copy_4_1_">
         <path d="M480,344.181L268.869,131.889c-15.756-15.859-41.3-15.859-57.054,0c-15.754,15.857-15.754,41.57,0,57.431l237.632,238.937   c8.395,8.451,19.562,12.254,30.553,11.698c10.993,0.556,22.159-3.247,30.555-11.698l237.631-238.937   c15.756-15.86,15.756-41.571,0-57.431s-41.299-15.859-57.051,0L480,344.181z"/>
       </g>
